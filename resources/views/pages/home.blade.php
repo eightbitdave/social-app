d2bbe4270caefb9c96fafd1fc63b4d8186c389c0
David Gray <16bitdave@gmail.com>
2015-03-23 19:23:58 +0000

    Changed jumbotron button on front page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,6 @@
 	<div class="jumbotron">
 	  <h1>Hello, world!</h1>
 	  <p>Code Soda helps you learn programming concepts by use of social interaction with other students!</p>
-	  <p><a class="btn btn-success btn-lg" href="#" role="button">Learn more</a></p>
+	  <p><a class="btn btn-primary btn-lg" href="/users/create" role="button">Join Now</a></p>
 	</div>
 @stop
