aba48c9c73f2cace67376ccee0bd56b4d1faf4b7
David Gray <16bitdave@gmail.com>
2015-03-23 15:11:06 +0000

    Changed branding

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div class="jumbotron">
 	  <h1>Hello, world!</h1>
-	  <p>EduCode helps you learn programming concepts by use of social interaction with other students!</p>
+	  <p>Code Soda helps you learn programming concepts by use of social interaction with other students!</p>
 	  <p><a class="btn btn-success btn-lg" href="#" role="button">Learn more</a></p>
 	</div>
 @stop
