24fdcfe1fb5df060c1860d20dc776cd6e4b02ade
David Gray <16bitdave@gmail.com>
2015-03-26 10:26:44 +0000

    Added dashboard functionality

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
 	<h3 id="page-title">Dashboard</h3>
+
+	<div class="btn-group btn-group-justified" role="group" aria-label="...">
+	  <div class="btn-group" role="group">
+	    <a class="btn btn-default" href="/posts">Posts</a>
+	  </div>
+	  <div class="btn-group" role="group">
+	    <a class="btn btn-default" href="/groups">Groups</a>
+	  </div>
+	  <div class="btn-group" role="group">
+	    <a class="btn btn-default" href="/auth/logout">Logout</a>
+	  </div>
+	</div>
 @stop
